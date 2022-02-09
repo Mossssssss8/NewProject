801b1653e589a712443e0c9ade203bfddf705da7
Mossssssss8 <77313349+Mossssssss8@users.noreply.github.com>
2022-02-10 00:13:15 +0700

    แก้หน้าต่าง Register And Login

---
+++ หน้าต่าง Login/api.js
@@ -0,0 +1,31 @@
+const UserApi = "http://localhost:3000/user/"
+async function UserRegister() {
+    var modeluser = {}
+    modeluser.username = document.getElementById("users").value
+    modeluser.age = document.getElementById("age").value
+    modeluser.gender = document.getElementById("Gender").value
+    modeluser.Height = document.getElementById("Height").value
+    modeluser.Weight = document.getElementById("Weight").value
+    modeluser.email = document.getElementById("email").value
+    modeluser.password = document.getElementById("password").value
+    console.log(modeluser)
+    const response = await fetch(UserApi + "add", {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(modeluser)
+
+    })
+    return response.json()
+}
+async function getAllUser() {
+    var response = await fetch(UserApi, {
+        method: 'GET',
+
+    })
+    var allUSer = await response.json()
+    console.log(allUSer)
+    return response.json()
+}
---
+++ หน้าต่าง Login/data.js
@@ -0,0 +1,19 @@
+// var firebaseConfig = {
+//     apiKey: "AIzaSyBcJAmQeQvFdvziof1nEwgWe8-3UeB4YR4",
+//     authDomain: "project-login-1aced.firebaseapp.com",
+//     projectId: "project-login-1aced",
+//     storageBucket: "project-login-1aced.appspot.com",
+//     messagingSenderId: "203879694250",
+//     appId: "1:203879694250:web:7bdb49456a994e6008f5d3"
+// };
+// firebase.initializeApp(firebaseConfig);
+
+// var datab = firebase.database().ref('data');
+// var email = document.getElementById('email').value;
+// var password = document.getElementById('password').value;
+// firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+
+// }).catch(function(error) {
+//     var errorcode = error.code;
+
+// })
