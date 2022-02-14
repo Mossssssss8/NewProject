656b78a0490cb13a9cf5e0b33449060492b6d950
Luckyjamess <6106021630023@fitm.kmutnb.ac.th>
2022-02-14 19:05:23 +0700

    เพิ่มการ login แล้วให้คืนค่า User ที่ Login มา และเพิ่มการกด Enter ในการ Login ปรับตำแหน่งการ Link ของ Api.js

--- หน้าต่าง Login/api.js
+++ หน้าต่าง Login/api.js
@@ -1,3 +1,17 @@
+// เพิ่มการกด Enter ใน Tag button
+var passwordInput = document.getElementById("passwordForLogin");
+passwordInput.addEventListener("keyup", function(event) {
+    console.log(event)
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("Login_Btn").click();
+    }
+  });
+//
+
 const UserApi = "http://localhost:3000/user/"
 async function UserRegister() {
     var modeluser = {}
@@ -44,6 +58,9 @@ async function login() {
     })
     var res = await response.json()
     if (res) {
+        //ถ้า Login สำเร็จ
+        console.log(res)
+        alert("Alert")
         location.href = "../Responsive Sidebar menu/index.html"
     } else {
         Swal.fire(
